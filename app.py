9816f9d7e9dabb7b10658fd17755bf0a1a6fc0c5
unknown <iden.ticlla@gmail.com>
2021-08-09 09:23:54 -0400

    add Create new task and list all tasks

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from logging import debug
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from werkzeug.utils import redirect
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -18,9 +19,20 @@ class Todo(db.Model):
         return f"<Task {self.id}>"
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        task_content = request.form.get("content")
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
 
 if __name__ == "__main__":
     app.run(debug=True)
